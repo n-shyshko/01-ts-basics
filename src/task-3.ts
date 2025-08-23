f7668bd64da797f52175cca3a69663ba68152920
nik <nshishko@gmail.com>
2025-08-23 12:49:19 +0300

    4th

--- src/task-3.ts
+++ src/task-3.ts
@@ -1,8 +1,13 @@
-const usernames = ["alice", "bob", "charlie"];
+const usernames: string[] = ["alice", "bob", "charlie"];
 
-const ratings = [4.5, 3.8, 5];
+const ratings: number[] = [4.5, 3.8, 5];
 
-const products = [
+interface Product {
+  id: number;
+  title: string;
+}
+
+const products: Product[] = [
   { id: 1, title: "Phone" },
   { id: 2, title: "Laptop" },
 ];
